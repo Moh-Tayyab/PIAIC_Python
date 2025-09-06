0c02908a832d416dbe00a7072c647e213498d110
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-06 23:05:04 +0500

    updation

---
+++ class_content/class_03/main.py
@@ -0,0 +1,15 @@
+
+percentage = int(input("please enter your percentage: "))
+
+if percentage >= 90 and percentage <= 100:
+    print("Your grade is A+")
+elif percentage >= 80 and percentage < 90:
+    print("Your grade is A")
+elif percentage >= 70 and percentage < 80:
+    print("your grade is B")
+elif percentage >= 60 and percentage < 70:
+    print("your grade is C")
+else: 
+    print("sorry try again")
+    
+            
