9b1a2d0d465e12b2ed48a08eb28f8e55dea75fcf
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-07 22:15:23 +0500

    practice sort method optional parameters

--- class_content/class_04/main.py
+++ class_content/class_04/main.py
@@ -36,7 +36,30 @@ print(f"Add an element at specified position: {num_list}")
 
 #pop 
 #Removes the element at the specified position
-num_list.pop(0) #if you do not provide index it will remove last element
+#if you do not provide index it will remove last element
+#if you provide index it will remove that element
+num_list.pop(0) #index
 print(f"Removes the element at the specified position: {num_list}")
 
 #remove
+#Removes the item with the specified value
+num_list.remove(3) #value
+print(f"Removes the item with the specified value: {num_list}")
+
+#sort
+#Sorts the list
+num_list.sort()
+print(f"Sorts the list: {num_list}")
+
+#Sort have optional parameters:
+#reverse=True → descending order
+#key=function → custom sorting logic
+
+
+num_list.sort(reverse=True)
+print(f"{num_list}")
+
+fruit_list = ["apple", "banana", "cherry", "kiwi"]
+# Sort by length of word
+fruit_list.sort(key=len)
+print(f"Sort by length of word: {fruit_list}")
