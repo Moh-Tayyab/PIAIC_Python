62a96552ad5b3a79913b129263dcd126c7a10e56
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-07 22:01:14 +0500

    updation

--- class_content/class_04/main.py
+++ class_content/class_04/main.py
@@ -12,3 +12,31 @@ print(num_list[1]) #2
 print(num_list[2]) #3
 print(num_list[3]) #4
 print(num_list[4]) #5
+
+#List Methods
+#append
+#insert
+#remove
+#pop
+#sort
+#reverse
+#len
+
+
+#append
+#Adds an element at the end of the list
+num_list.append(6)
+print(f"Add an element at end of list: {num_list}")
+
+#insert
+#Adds an element at the specified position
+num_list.insert(0,0)# index, value
+print(f"Add an element at specified position: {num_list}")
+
+
+#pop 
+#Removes the element at the specified position
+num_list.pop(0) #if you do not provide index it will remove last element
+print(f"Removes the element at the specified position: {num_list}")
+
+#remove
