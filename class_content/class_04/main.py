06ff79716d92149006a73ed3ad1cba976a4f6fdc
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-07 21:51:04 +0500

    list & indexing

---
+++ class_content/class_04/main.py
@@ -0,0 +1,14 @@
+#List
+# A list is a collection which is ordered and changeable. Allows duplicate members.
+# Lists are written with square brackets.
+# Example:
+#index      0 1 2 3 4
+num_list = [1,2,3,4,5]
+print(num_list)
+
+#indexing
+print(num_list[0]) #1
+print(num_list[1]) #2
+print(num_list[2]) #3
+print(num_list[3]) #4
+print(num_list[4]) #5
