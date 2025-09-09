1c53a3b5c3902220eec0c97be98a7112a25790fa
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-10 00:27:47 +0500

    updation

--- class_content/class_03/main.py
+++ class_content/class_03/main.py
@@ -19,26 +19,35 @@ user = input("Please Enter Your Role: ").lower()
  
 password = input("Please enter your password according to your role: ")
  
-if user == "admin" and password == "123":
+if user in "admin" and password in "123":
     print("welcome admin")
-    if user != "admin":
-        print("please check your role is incorrect")
-    elif password != "123":
-        print("your password is wrong")
+    if user != "admin" and password in "123":
+        print(f"your role {user} is incorrect. your password {password} is correct but role {user} is incorrect.")
+    elif password != "123" and user in "admin":
+        print(f"your password {password} is wrong. your role {user} is correct but your {password} is incorrect.")
     
-elif user == "frontend developer" and password == "456":
+elif user in "frontend developer" and password in "456":
       print(f"Welcome {user} and your pasword is {password}")
-      if user != "frontend developer":
-          print("please check role your is incorrect ")
-      elif password == "456" and user != "frontend developer":
-          print(f"your password is {password} correct but your role is not correct!")    
-      elif password != "456":
-          print(f"role is {user}. your password is wrong")    
+      if user != "frontend developer" and password in "456":
+          print(f"your role {user} is incorrect. your password {password} is correct but role {user} is incorrect.")
+      elif password in "456" and user != "frontend developer":
+          print(f"your password {password} is wrong. your role {user} is correct but your {password} is incorrect.") 
+          
+elif user in "backend developer" and password in "789":
+    print(f"Welcome {user} and your password is {password}")
+    if user != "backend developer" and password in "789":
+        print(f"your role {user} is incorrect. your password {password} is correct but role {user} is incorrect.")
+    elif password != "789" and user in "backed developer":
+        print(f"your password {password} is wrong. your role {user} is correct but your {password} is incorrect.")
     
-elif user == "backend developer" and password == "789":
+elif user in "graphic designer" and password in "1011":
     print(f"Welcome {user} and your password is {password}")
-elif user == "graphic designer" and password == "1011":
-    print(f"Welcome {user} and your password is {password}")
+    if user != "graphic designer" and password in "1011":
+        print(f"your role {user} is incorrect. your password {password} is correct but role {user} is incorrect.")
+    elif password != "1011" and user in "graphic designer":
+        print(f"your password {password} is wrong. your role {user} is correct but your {password} is incorrect.")
+        
+    
 else:
     print("Something went wrong.Please enter correct user role and password.")           
     
