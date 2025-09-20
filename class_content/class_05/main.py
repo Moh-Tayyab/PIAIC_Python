975f7c516c32580dc01547bdd9e39f1e53a29195
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-20 23:28:13 +0500

    python basic function update

--- class_content/class_05/main.py
+++ class_content/class_05/main.py
@@ -1,6 +1,4 @@
 # functions
-
-
 def sum(num1, num2):#parameters 
     
     num1 + num2
@@ -8,3 +6,10 @@ def sum(num1, num2):#parameters
     return f"The Sum of {num1} and {num2} is equal to {total}"
 
 print(sum(10, 20))
+
+def greet(message: str) -> str:
+    message = "hello how are you?"
+    
+    return f"{message}"
+
+print(greet("ello"))
