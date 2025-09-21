5f34a7737ebd91ff504af7062ebffb8da73e6ea9
Moh-Tayyab <m.tayyab1263@gmail.com>
2025-09-21 23:56:19 +0500

    updation

---
+++ class_content/class_06/main.py
@@ -0,0 +1,15 @@
+# lamda function and dictionary
+
+def sum(num1, num2):
+    
+    return num1 + num2
+
+print(sum(1, 6))
+    
+# lambda num_1, num_2:
+# add = num_1 + num_2
+
+# print(add(3,5))
+    
+    
+    
